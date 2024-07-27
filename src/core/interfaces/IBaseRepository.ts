6e0ab95d5e06c2cc8b343580c778cc67dd567deb
Kerolous Amged <Kokp.amged@gmail.com>
2024-07-27 20:31:38 +0300

    first commit

---
+++ src/core/interfaces/IBaseRepository.ts
@@ -0,0 +1,11 @@
+export interface IBaseRepository<T> {
+  getAll(): Promise<T[]>;
+
+  getById(id: number): Promise<T>;
+
+  create(entity: T): Promise<T>;
+
+  update(entity: T): Promise<T>;
+
+  delete(id: number): Promise<T>;
+}
